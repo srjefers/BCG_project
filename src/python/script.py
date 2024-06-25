ad88fc233b25f3354fa801f1cdd0535f2916b7bc
Jeferson Argueta <ajefersonstiv@gmail.com>
2024-06-25 02:06:40 -0600

    Project Env & SQLite data
    * Project has a yaml env file
    * SQLite database has been created through python and pandas
    * Data has been cleaned and modeled to be able to be inserted at dim and
      facts

--- src/python/script.py
+++ src/python/script.py
@@ -9,11 +9,17 @@ def fn_execute_api_call():
     """
     load_dotenv()
     api_token = os.getenv("API_Token")
-    uri = "https://api.football-data.org/v4/competitions/CL/matches"
+    # uri = "https://api.football-data.org/v4/competitions/"
+    uri = "https://api.football-data.org/v4/teams/"
+    """
+        /v4/competitions/{id}/teams
+        List all teams for a particular competition.
+    """
     headers = {'X-Auth-Token': api_token}
 
     response = requests.get(uri,headers=headers)
-    for match in response.json()['matches']:
+    # for match in response.json()['competitions']:
+    for match in response.json()['teams']:
         print(match)
 
 def main():
